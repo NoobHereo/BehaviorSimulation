84814fc54872cac1a03f4cee64bfab52cb911a97
NoobHereo <NoobHereo@users.noreply.github.com>
2020-10-10 02:19:34 +0200

    Added logging

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Globalization;
+
+using log4net;
+using log4net.Config;
 
 namespace BehaviorSimulation
 {
     class Program
     {
+        static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+
+            log.Info("XML configurated...");
+
+            while (true)
+                Console.ReadLine();
         }
     }
 }
